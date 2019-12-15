2c43a551496589461955b5f143734c5e5a5b880a
Manuel <e1426298@student.tuwien.ac.at>
2019-12-15 18:38:13 +0100

    fix: use showOutputButtons() and .trim() on language switch

--- src/components/Converter.vue
+++ src/components/Converter.vue
@@ -9,7 +9,7 @@
                           variant="outline-secondary"
                           style="white-space: nowrap"
                           @click="switchLanguages()"
-                    >
+                >
                     <fa-icon :icon="['fas', 'arrow-left']" class="mb-1" /> Switch <fa-icon :icon="['fas', 'arrow-right']" class="mb-1" />
                 </b-button>
             </div>
@@ -24,15 +24,15 @@
                               language="css"
                               line-numbers
                               @change="inputChange"
-                    />
+                />
             </div>
             <div class="editor output-editor copy-all">
-                <div v-if="output && output !== MALFORMED_INPUT_OUTPUT" class="output-buttons">
+                <div v-if="showOutputButtons" class="output-buttons">
                     <b-button title="Download as file"
                               squared
                               variant="primary"
                               @click="downloadOutputAsFile()"
-                        >
+                    >
                         <fa-icon :icon="['fas', 'download']" />
                     </b-button>
                     <b-button class="clipboard-button"
@@ -40,7 +40,7 @@
                               squared
                               variant="primary"
                               @click="copyOutputToClipboard()"
-                        >
+                    >
                         <fa-icon :icon="['fas', 'clipboard']" />
                     </b-button>
                 </div>
@@ -49,7 +49,7 @@
                              variant="success"
                              auto-hide-delay="1000"
                              static
-                        >
+                    >
                         <strong>Copied to clipboard</strong> <fa-icon :icon="['fas', 'check']" class="mb-1" />
                     </b-toast>
                 </div>
@@ -57,83 +57,85 @@
                               emit-events
                               language="css"
                               readonly
-                    />
+                />
             </div>
         </div>
     </div>
 </template>
 
 <script lang="ts">
-import { Component, Vue } from 'vue-property-decorator';
-import { convertSassToScss } from '@/util/convertSassToScss';
-import { convertScssToSass } from '@/util/convertScssToSass';
-import { formatSass } from '@/util/formatSass';
-import { formatScss } from '@/util/formatScss';
-import { downloadTextAsFile } from '@/util/downloadTextAsFile';
+    import { Component, Vue } from 'vue-property-decorator';
+    import { convertSassToScss } from '@/util/convertSassToScss';
+    import { convertScssToSass } from '@/util/convertScssToSass';
+    import { formatSass } from '@/util/formatSass';
+    import { formatScss } from '@/util/formatScss';
+    import { downloadTextAsFile } from '@/util/downloadTextAsFile';
 
-@Component<Converter>({
-    watch: {
-        inputLanguage() {
-            localStorage.setItem('inputLanguage', this.inputLanguage);
+    @Component<Converter>({
+        watch: {
+            inputLanguage() {
+                localStorage.setItem('inputLanguage', this.inputLanguage);
+            },
         },
-    },
-})
-export default class Converter extends Vue {
+    })
+    export default class Converter extends Vue {
 
-    public MALFORMED_INPUT_OUTPUT = 'malformed input...';
+        public MALFORMED_INPUT_OUTPUT = 'malformed input...';
 
-    public inputLanguage: 'Sass' | 'SCSS' = 'Sass';
+        public inputLanguage: 'Sass' | 'SCSS' = 'Sass';
 
-    public input: string = '';
+        public input: string = '';
 
-    get outputLanguage(): 'Sass' | 'SCSS' {
-        return this.inputLanguage === 'Sass' ? 'SCSS' : 'Sass';
-    }
-
-    public output: string = '';
-
-    created() {
-        const storedInputLanguage = localStorage.getItem('inputLanguage');
-        if (storedInputLanguage === 'Sass' || storedInputLanguage === 'SCSS') {
-            this.inputLanguage = storedInputLanguage;
+        get outputLanguage(): 'Sass' | 'SCSS' {
+            return this.inputLanguage === 'Sass' ? 'SCSS' : 'Sass';
         }
-    }
 
-    async switchLanguages() {
-        this.inputLanguage = this.outputLanguage;
-        this.input = this.output === this.MALFORMED_INPUT_OUTPUT ? '' : this.output;
-        await this.$nextTick();
-        this.inputChange();
-    }
+        public output: string = '';
 
-    async inputChange() {
+        get showOutputButtons(): boolean {
+            return !!(this.output && this.output.trim() && this.output !== this.MALFORMED_INPUT_OUTPUT);
+        }
 
-        this.input = `${this.input.trim()}\n\n`;
-
-        try {
-            if (this.inputLanguage === 'Sass') {
-                this.output = await convertSassToScss(this.input);
-            }
-            else {
-                this.output = await convertScssToSass(this.input);
+        created() {
+            const storedInputLanguage = localStorage.getItem('inputLanguage');
+            if (storedInputLanguage === 'Sass' || storedInputLanguage === 'SCSS') {
+                this.inputLanguage = storedInputLanguage;
             }
         }
-        catch (e) {
-            this.output = this.MALFORMED_INPUT_OUTPUT;
+
+        async switchLanguages() {
+            this.inputLanguage = this.outputLanguage;
+            this.input = (this.output === this.MALFORMED_INPUT_OUTPUT ? '' : this.output).trim();
+            await this.$nextTick();
+            this.inputChange();
+        }
+
+        async inputChange() {
+
+            try {
+                if (this.inputLanguage === 'Sass') {
+                    this.output = await convertSassToScss(`${this.input.trim()}\n\n`);
+                }
+                else {
+                    this.output = await convertScssToSass(`${this.input.trim()}\n\n`);
+                }
+            }
+            catch (e) {
+                this.output = this.MALFORMED_INPUT_OUTPUT;
+            }
+
+        }
+
+        async copyOutputToClipboard() {
+            await this.$copyText(this.output);
+            (this as any).$bvToast.show('clipboard-toast');
+        }
+
+        downloadOutputAsFile() {
+            downloadTextAsFile(`style.${this.outputLanguage}`, this.output);
         }
 
     }
-
-    async copyOutputToClipboard() {
-        await this.$copyText(this.output);
-        (this as any).$bvToast.show('clipboard-toast');
-    }
-
-    downloadOutputAsFile() {
-        downloadTextAsFile(`style.${this.outputLanguage}`, this.output);
-    }
-
-}
 </script>
 
 <!-- Add "scoped" attribute to limit CSS to this component only -->
