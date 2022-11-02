5109bac4d41e31b5f2dd1fed47d5bef9fccddcbf
Manuel Schüller <manuel@manuelschueller.at>
2022-11-02 19:48:12 +0100

    chore: remove dead code

--- src/util/interpolationHack.ts
+++ src/util/interpolationHack.ts
@@ -2,13 +2,11 @@ export function interpolationHack(child: any) {
   if (child.type === 'interpolation') {
     child.children.unshift({
       type: 'space',
-      position: {},
       value: '#{',
     });
 
     child.children.push({
       type: 'space',
-      position: {},
       value: '}',
     });
   }
--- src/util/traverseAst.ts
+++ src/util/traverseAst.ts
@@ -1,11 +1,6 @@
-// let level = 0;
-
 export function traverseAst(root: any, forEveryNode: (child: any) => void) {
-  // console.log(`.${'  '.repeat(level)}`,root);
-  // level++;
   forEveryNode(root);
   if (root && root.children && root.children.length > 0) {
     root.children.forEach((child: any) => traverseAst(child, forEveryNode));
   }
-  // level--;
 }
