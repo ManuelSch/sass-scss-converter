011e7471f35a98e779f4c6df7ae55dd9ba9ffc27
Manuel Schüller <manuel@manuelschueller.at>
2023-07-07 21:08:34 +0200

    feat: add 'Buy Me a Coffee' button

--- src/components/Navbar.vue
+++ src/components/Navbar.vue
@@ -1,23 +1,33 @@
 <template>
-    <b-navbar type="dark" variant="primary">
-        <b-navbar-brand>
-            Sass-to-SCSS Converter
-            [Beta]
-            <a class="made-by" href="https://github.com/ManuelSch/sass-scss-converter">
-                by ManuelSch
-            </a>
-        </b-navbar-brand>
-        <b-navbar-nav class="ml-auto">
-            <b-button href="https://github.com/ManuelSch/sass-scss-converter/issues">
-                <fa-icon :icon="['fas', 'exclamation-triangle']" />
-                Report a bug
-            </b-button>
-            <b-button href="https://github.com/ManuelSch/sass-scss-converter" style="color: white" variant="link">
-                <fa-icon :icon="['fab', 'github']" />
-                View sources
-            </b-button>
-        </b-navbar-nav>
-    </b-navbar>
+  <b-navbar type="dark" variant="primary">
+    <b-navbar-brand>
+      Sass-to-SCSS Converter
+      [Beta]
+      <a class="made-by" href="https://github.com/ManuelSch/sass-scss-converter">
+        by ManuelSch
+      </a>
+    </b-navbar-brand>
+    <b-navbar-nav class="ml-auto">
+      <b-button href="https://github.com/ManuelSch/sass-scss-converter/issues">
+        <fa-icon :icon="['fas', 'exclamation-triangle']"/>
+        Report a bug
+      </b-button>
+      <b-button href="https://github.com/ManuelSch/sass-scss-converter" style="color: white"
+                variant="link">
+        <fa-icon :icon="['fab', 'github']"/>
+        View sources
+      </b-button>
+      <a href="https://ko-fi.com/Q5Q3MZNCY" target="_blank">
+        <img
+          height="36"
+          style="border:0; height:36px;"
+          src="https://storage.ko-fi.com/cdn/kofi1.png?v=3"
+          border="0"
+          alt="Buy Me a Coffee at ko-fi.com"
+        />
+      </a>
+    </b-navbar-nav>
+  </b-navbar>
 </template>
 
 <script lang="ts">
@@ -31,8 +41,8 @@ export default class Navbar extends Vue {
 
 <style lang="sass">
 .made-by
-    color: white
-    font-size: 70%
-    opacity: 0.7
-    margin-left: .5rem
+  color: white
+  font-size: 70%
+  opacity: 0.7
+  margin-left: .5rem
 </style>
